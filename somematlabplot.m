1123d496d66c995d7ab6d0e19fce6dcef0bf1db4
Kevin Alexander Höfle <kevinahoefle@gmail.com>
2014-07-21 21:53:34 +0200

    Update somematlabplot.m

--- somematlabplot.m
+++ somematlabplot.m
@@ -7,11 +7,11 @@ for i = 1:length(A)
        A_new(x) = A(i);
     end
     
-    
 end
+
 [nb,xb] = hist(A_new,10000);
 bh=bar(xb,nb);
-set(bh,'facecolor',[1 0 0]);
+set(bh,'facecolor',[0 0 0]);
 hold on;
 
 B = dlmread('../HeatTrigger/CPU_old/fil4time_cpu_first.txt');
@@ -30,44 +30,110 @@ set(bh,'facecolor',[0 0 1]);
 hold on;
 
 C = dlmread('../HeatTrigger/CPU_old/fil8time_cpu_first.txt');
-[nb,xb] = hist(C,10000);
+x = 0;
+for i = 1:length(C)
+   
+    if(A(i) < 0.0012)
+      x = x +1; 
+       C_new(x) = C(i);
+    end
+    
+end
+
+[nb,xb] = hist(C_new,10000);
 bh=bar(xb,nb);
 set(bh,'facecolor',[0 1 0]);
 hold on;
 
 D = dlmread('../HeatTrigger/CPU_old/fil16time_cpu_first.txt');
-[nb,xb] = hist(D,10000);
+for i = 1:length(D)
+   
+    if(B(i) < 0.00012)
+      x = x +1; 
+       D_new(x) = D(i);
+    end
+    
+    
+end
+[nb,xb] = hist(D_new,10000);
 bh=bar(xb,nb);
 set(bh,'facecolor',[0 1 1]);
 hold on;
 
 E = dlmread('../HeatTrigger/CPU_old/fil32time_cpu_first.txt');
-[nb,xb] = hist(E,10000);
-bh=bar(xb,nb);
-set(bh,'facecolor',[1 0 0]);
-hold on;
+x = 0;
+for i = 1:length(E)
+   
+    if(A(i) < 0.0012)
+      x = x +1; 
+       E_new(x) = E(i);
+    end
+    
+end
 
-F = dlmread('../HeatTrigger/CPU_old/fil64time_cpu_first.txt');
-[nb,xb] = hist(F,10000);
+[nb,xb] = hist(E_new,10000);
 bh=bar(xb,nb);
 set(bh,'facecolor',[1 0 1]);
 hold on;
 
-G = dlmread('../HeatTrigger/CPU_old/fil128time_cpu_first.txt');
-[nb,xb] = hist(G,10000);
-bh=bar(xb,nb);
-set(bh,'facecolor',[1 1 0]);
-hold on;
-
-H = dlmread('../HeatTrigger/CPU_old/fil256time_cpu_first.txt');
-[nb,xb] = hist(H,10000);
+F = dlmread('../HeatTrigger/CPU_old/fil64time_cpu_first.txt');
+for i = 1:length(F)
+   
+    if(B(i) < 0.00012)
+      x = x +1; 
+       F_new(x) = F(i);
+    end
+    
+    
+end
+[nb,xb] = hist(F_new,10000);
 bh=bar(xb,nb);
 set(bh,'facecolor',[1 1 1]);
 hold on;
 
+G = dlmread('../HeatTrigger/CPU_old/fil128time_cpu_first.txt');
+x = 0;
+for i = 1:length(G)
+   
+    if(A(i) < 0.0012)
+      x = x +1; 
+       G_new(x) = G(i);
+    end
+    
+end
 
-I = dlmread('../HeatTrigger/CPU_old/fil256time_cpu_first.txt');
-[nb,xb] = hist(I,10000);
+[nb,xb] = hist(G_new,10000);
+bh=bar(xb,nb);
+set(bh,'facecolor',[1 0 1]);
+hold on;
+
+H = dlmread('../HeatTrigger/CPU_old/fi256time_cpu_first.txt');
+for i = 1:length(H)
+   
+    if(H(i) < 0.00012)
+      x = x +1; 
+       H_new(x) = H(i);
+    end
+    
+    
+end
+[nb,xb] = hist(H_new,10000);
+bh=bar(xb,nb);
+set(bh,'facecolor',[0 1 1]);
+hold on;
+
+
+I = dlmread('../HeatTrigger/CPU_old/fi512time_cpu_first.txt');
+for i = 1:length(I)
+   
+    if(I(i) < 0.00012)
+      x = x +1; 
+       I_new(x) = I(i);
+    end
+    
+    
+end
+[nb,xb] = hist(I_new,10000);
 bh=bar(xb,nb);
 set(bh,'facecolor',[0 0 1]);
 hold on;
